34360269cf3403d5890e1caaecad72c4a904b8cc
Dan Mopsick <danmopsick@gmail.com>
2022-11-09 20:23:17 -0500

    Checking in the assignment3 files

---
+++ Assignment3/client.py
@@ -0,0 +1,213 @@
+from multiprocessing import Value
+from threading import Timer
+from utils import States
+import multiprocessing
+import random
+import socket
+import time
+import utils
+
+UDP_IP = "127.0.0.1"
+# UDP_PORT = 5005 # Connecting without channel
+UDP_PORT = 5007
+MSS = 12 # maximum segment size | I am assuming that is measured in bytes
+MSL = 5 # MAX SEGMENT LIFETIME -  Not sure what value it should be. I am measuring this in seconds for use with Python Time Library
+
+def send_udp(message):
+  sock.sendto(message, (UDP_IP, UDP_PORT))
+
+class Client:
+  def __init__(self):
+    self.client_state = States.CLOSED
+    self.handshake()
+
+  def handshake(self):
+    if self.client_state == States.CLOSED:
+      seq_num = utils.rand_int()
+      syn_header = utils.Header(seq_num, 0, syn = 1, ack = 0, fin=0)
+      # for this case we send only header;
+      # if you need to send data you will need to append it
+      send_udp(syn_header.bits())
+
+      self.update_state(States.SYN_SENT)
+
+      # Initialize last received ack and seq num
+      self.last_received_ack = -1
+      self.last_received_seq_num = -1
+      
+      # Wait for response from syn_ack
+      self.receive_acks()
+
+      # print("Received ack " + str(self.last_received_ack))
+      # print("Sent seq num " + str(seq_num))
+
+      # Check if the received ack is equal to the sent seq_num + 1
+      if self.last_received_ack == seq_num + 1:
+        server_seq_num = self.last_received_seq_num 
+        ack_num = server_seq_num + 1
+        # I do not think ack needs a seq num
+        # ack_seq_num = utils.rand_int()
+
+        # Build the header to send the ACK
+        ack_header = utils.Header(0, ack_num, syn=0, ack=1, fin=0)
+
+        # Send the ACK
+        send_udp(ack_header.bits())
+
+        self.update_state(States.ESTAB)
+    
+    else:
+      pass
+
+  def terminate(self):
+
+    # Can we only terminate an established connection? 
+    if self.client_state == States.ESTAB:
+      # Generate Seq num
+      fin_seq_num = utils.rand_int()
+
+      # Build Fin header to send
+      fin_header = utils.Header(fin_seq_num, 0, syn=0, ack=0, fin=1)
+
+      # Send the fin header
+      send_udp(fin_header.bits())
+
+      self.update_state(States.FIN_WAIT_1)
+
+      # Wait to receive an ack
+      self.last_received_ack = -1
+      self.last_received_seq_num = -1
+      self.receive_acks()
+      
+      # Verify the received ack_num
+      if self.last_received_ack == fin_seq_num + 1: 
+        self.update_state(States.FIN_WAIT_2)
+
+        # Wait for the FIN from the sever
+        self.receive_acks()
+
+        # Load the received seq num
+        server_fin_seq_num =  self.last_received_seq_num
+        
+        fin_ack_num = server_fin_seq_num + 1
+
+        # Build a header to send an ACK back
+        fin_ack_header = utils.Header(0, fin_ack_num, syn=0, ack=1, fin=0)
+
+        # Send the ACK of the FIN to the server
+        send_udp(fin_ack_header.bits())
+
+        # Should the time wait be before the directly above sending of ACK? I think not based on the RFC diagram and slides
+        # Here is where we can do a timed wait for twice the legth of the max segment life time (MSL)
+        self.update_state(States.TIMED_WAIT)
+        time.sleep(MSL * 2)
+
+        # Close the connection
+        self.update_state(States.CLOSED)
+
+    else:
+      # print("Terminate called, but not in ESTAB state")
+      pass
+     
+  def update_state(self, new_state):
+    if utils.DEBUG:
+      print(self.client_state, '->', new_state)
+    self.client_state = new_state
+
+  def send_reliable_message(self, message):
+    # print("Send Reliable message called with " + message)
+
+    # Verify that the connection is established before we send the message
+    if self.client_state == States.ESTAB:
+      # send messages
+      # we loop/wait until we receive all ack.
+
+      # Going to treat each ASCII character as one byte    
+      # Need to determine how much of the message has been sent
+      charactersSent = 0
+
+      # Iterate until the entire messae is sent
+      while charactersSent  < len(message):
+        # Build header
+        transferHeader = utils.Header(0, 0, 0, 0, 0)
+        
+        # Build the portion of the body to send
+        transferBody = message[charactersSent:charactersSent + MSS]
+
+        # print("Raw text to send " + transferBody)
+
+        # Convert the ascii text to bits
+        transferBodyBits = transferBody.encode('ascii')
+
+        # print('Bits to send ' + str(transferBodyBits))
+
+        # Combine header and the body and send it
+        send_udp(transferHeader.bits() + transferBodyBits)
+
+        # Increment characters sent 
+        charactersSent += MSS
+
+
+  # these two methods/function can be used receive messages from
+  # server. the reason we need such mechanism is `recv` blocking
+  # and we may never recieve a package from a server for multiple
+  # reasons.
+  # 1. our message is not delivered so server cannot send an ack.
+  # 2. server responded with ack but it's not delivered due to
+  # a network failure.
+  # these functions provide a mechanism to receive messages for
+  # 1 second, then the client can decide what to do, like retransmit
+  # if not all packets are acked.
+  # you are free to implement any mechanism you feel comfortable
+  # especially, if you have a better idea ;)
+  def receive_acks_sub_process(self, s, lst_rec_ack_shared, lst_rec_seq_shared):
+    if utils.DEBUG:
+      print('subproc start in function')
+    while True:
+      recv_data, addr = s.recvfrom(1024)
+      header = utils.bits_to_header(recv_data)
+      if utils.DEBUG:
+        print('received subproc header: ')
+        print(header)
+      if header.ack_num > lst_rec_ack_shared.value:
+        lst_rec_ack_shared.value = header.ack_num
+        if utils.DEBUG:
+          print('subproc updated ack: ', lst_rec_ack_shared.value)
+      if header.seq_num > lst_rec_seq_shared.value:
+        lst_rec_seq_shared.value = header.seq_num
+        if utils.DEBUG:
+          print('subproc updated seq: ', lst_rec_seq_shared.value)
+
+  def receive_acks(self):
+    # Start receive_acks_sub_process as a process
+    lst_rec_ack_shared = Value('i', self.last_received_ack)
+    lst_rec_seq_shared = Value('i', self.last_received_seq_num)
+
+    p = multiprocessing.Process(target=self.receive_acks_sub_process, args=(sock, lst_rec_ack_shared, lst_rec_seq_shared))
+    p.start()
+    # Wait for 1 seconds or until process finishes
+    p.join(1)
+    # If process is still active, we kill it
+    if p.is_alive():
+      p.terminate()
+      p.join()
+    # here you can update your client's instance variables.
+    self.last_received_ack = lst_rec_ack_shared.value
+    self.last_received_seq_num = lst_rec_seq_shared.value
+    if utils.DEBUG:
+      print('received: ', self.last_received_ack)
+      print('shared variable: ', lst_rec_ack_shared)
+
+# Need to have a main process to use the multi threading logic
+if __name__ == "__main__":
+  sock = socket.socket(socket.AF_INET,    # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+  # we create a client, which establishes a connection
+  client = Client()
+
+  # we send a message
+  client.send_reliable_message("The quick brown fox jumps over the lazy dog")
+
+  # we terminate the connection
+  client.terminate()
---
+++ Assignment3/server.py
@@ -0,0 +1,137 @@
+import socket
+from turtle import update
+import utils
+from utils import States
+
+UDP_IP = "127.0.0.1"
+# UDP_PORT = 5005 # For testing without channel
+UDP_PORT = 5008
+
+# initial server_state
+server_state = States.CLOSED
+
+# Build the UDP socket
+sock = socket.socket(socket.AF_INET,    # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+sock.bind((UDP_IP, UDP_PORT)) # wait for connection
+
+# Some helper functions to keep the code clean and tidy
+def update_server_state(new_state):
+  global server_state
+  if utils.DEBUG:
+    print(server_state, '->', new_state) 
+  server_state = new_state
+
+# Receive a message and return header, body and addr
+# addr is used to reply to the client
+# this call is blocking
+def recv_msg():
+  data, addr = sock.recvfrom(1024)
+  header = utils.bits_to_header(data)
+  body = utils.get_body_from_data(data)
+  return (header, body, addr)
+
+# Variable to hold the entire message
+receivedMessage = ""
+
+# the server runs in an infinite loop and takes
+# action based on current state and updates its state
+# accordingly
+# You will need to add more states, please update the possible
+# states in utils.py file
+while True:
+  if server_state == States.CLOSED:
+    # we already started listening, just update the state
+    update_server_state(States.LISTEN)
+  elif server_state == States.LISTEN:
+    # we are waiting for a message
+    header, body, addr = recv_msg()
+    # if received message is a syn message, it's a connection initiation 
+    if header.syn == 1:
+      # Set the sequence to a random number 
+      seq_number = utils.rand_int() 
+
+      # ACK is the incoming header SEQ number + 1
+      ack_number = header.seq_num + 1
+
+      # Update server state to SYN_RECEIVED
+      update_server_state(States.SYN_RECEIVED)
+
+      ### sending message from the server:
+      #   use the following method to send messages back to client
+      #   addr is recieved when we receive a message from a client (see above)
+      #   sock.sendto(your_header_object.bits(), addr)
+
+  elif server_state == States.SYN_RECEIVED:
+    # Received a SYN... Need to reply with a SYN_ACK
+
+    # Create a header
+    syn_ack_header = utils.Header(seq_number, ack_number, syn=1, ack=1, fin=0)
+
+    # Send the syn ack
+    # Can I do this here? Will I have access to the addr variable? Or do I send above 
+    sock.sendto(syn_ack_header.bits(), addr)
+
+    # Modify the state
+    update_server_state(States.SYN_SENT)
+
+  elif server_state == States.SYN_SENT:
+    # Wait to receive an ack back from the client
+    header, body, addr = recv_msg()
+
+    # Check if the client replied with an ack
+    if header.ack == 1:
+      # print("CLIENT REPLIED WITH ACK")
+      update_server_state(States.ESTAB)
+  elif server_state == States.ESTAB:
+    # Need to receive messages until the state changes
+    header, body, addr = recv_msg()
+    
+    # Check if there is a fin bit to end the connection
+    if header.fin == 1:
+      # Load the received seq_num
+      fin_seq_num = header.seq_num
+      fin_ack_num = fin_seq_num + 1
+
+      fin_ack_header = utils.Header(0, fin_ack_num, syn=0, ack=1, fin=0)
+      
+      # Send the FIN ACK to the client
+      sock.sendto(fin_ack_header.bits(), addr)
+
+      update_server_state(States.CLOSE_WAIT)
+
+      # Leaving this print to show the transferred message
+      print("FINAL LOADED MESSAGE " +  receivedMessage)
+    else:
+      # No FIN bit, continue loading the message segment by segment
+      # print("Flag 1 - Received segment: " + body)
+
+      # Add the body of the TCP segment to the received message
+      receivedMessage = receivedMessage + body
+
+      # print("FLAG 10 - Entire message so far: " + receivedMessage)
+  elif server_state == States.CLOSE_WAIT:
+    # Generate a sequence num
+    seq_num = utils.rand_int()
+
+    # Build a FIN header to send to the the client
+    fin_header = utils.Header(seq_num, 0, syn=0, ack=0, fin=1)
+
+    # Send the fin header to the client
+    sock.sendto(fin_header.bits(), addr)
+
+    # Update to LAST_ACK
+    update_server_state(States.LAST_ACK)
+
+  elif server_state == States.LAST_ACK:
+    # Receive the final ack to finish the teardown
+    header, body, addr = recv_msg()
+
+    # Make sure the client as sent an ACK back
+    if header.ack == 1:
+    # Set state to closed, connection done, toredown, return server to closed
+      update_server_state(States.CLOSED)
+
+  else:
+    pass
